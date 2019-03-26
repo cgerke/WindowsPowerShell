9b7bf8e09f1715145a896282a0d63646b9cc900b
Chris <chris.gerke@gmail.com>
2019-03-26 12:41:32 +0800

    Defaults
    Just playing with best way to set preferences.

--- Microsoft.PowerShell_preferences.ps1
+++ Microsoft.PowerShell_preferences.ps1
@@ -1,5 +1,3 @@
-# User context Domain/Workgroup, but this assume Workgroup is WORKGROUP so make this smarter
-$DomainObj = switch ((Get-WmiObject Win32_ComputerSystem).Domain) {
-    "WORKGROUP" { (Get-WmiObject Win32_ComputerSystem).Name } default { (Get-WmiObject Win32_ComputerSystem).Domain }
+$global:Default = [pscustomobject]@{
+    Username = 'cgerke'
 }
-$PshAs = "$DomainObj\cgerke"
