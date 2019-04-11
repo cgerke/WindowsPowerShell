73a067cfd97057d04ca1bfa32737231b84887316
Chris <chris.gerke@gmail.com>
2019-04-11 21:57:47 +0800

    Debug
    Temp file for ideas and debugging.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -394,18 +394,3 @@ function prompt {
     "`n$('PS>' * ($nestedPromptLevel + 1)) "
 }
 #endregion prompt
-
-
-
-#region testing
-# consuming json
-<# $json = Join-Path -Path $PSDirectory -ChildPath "Microsoft.PowerShell_options.json"
-if ( Test-Path -path $json ) {
-    $Defaults = Get-Content $json | ConvertFrom-Json
-    #$JsonObject.Defaults[0]
-    #$Defaults.AdminAccount[0].Username
-} #>
-
-#self executing function
-#& { param($msg) Write-Host $msg } "Hello World"
-#endregion testing
---
+++ debug.ps1
@@ -0,0 +1,30 @@
+
+$File = "C:\Users\$env:UserName\AppData\Roaming\Jabra Direct\Devices.txt"
+
+If ( Test-Path -Path $File ){
+    $Hash = @{}
+
+    [System.IO.File]::ReadLines($File) | ForEach-Object {
+        If ( $_ | Select-String "Product Name:"){
+            $k = $_.split(':')
+        }
+        If ( $_ | Select-String "Serial Number:"){
+            $v = $_.split(':')
+            $Hash = $Hash + @{$k[1] = $v[1] }
+        }
+    }
+
+    $Hash
+}
+#region testing
+# consuming json
+<# $json = Join-Path -Path $PSDirectory -ChildPath "Microsoft.PowerShell_options.json"
+if ( Test-Path -path $json ) {
+    $Defaults = Get-Content $json | ConvertFrom-Json
+    #$JsonObject.Defaults[0]
+    #$Defaults.AdminAccount[0].Username
+} #>
+
+#self executing function
+#& { param($msg) Write-Host $msg } "Hello World"
+#endregion testing
