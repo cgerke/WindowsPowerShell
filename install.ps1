ff137fa80da41d70b8e782e9a90b176f39e47a16
Chris <chris.gerke@gmail.com>
2019-07-02 13:21:16 +0800

    Update install.ps1
    Install PSSudo for now.

--- install.ps1
+++ install.ps1
@@ -20,7 +20,7 @@
 }
 
 # Modules
-"PowerShellGet","posh-git","PSScriptAnalyzer","Plaster" | ForEach-Object -process {
+"PowerShellGet","posh-git","PSScriptAnalyzer","Plaster","PSSudo" | ForEach-Object -process {
   if (-not (Get-Module -ListAvailable -Name "$_")) {
     Install-Module "$_" -Scope CurrentUser -Force -Confirm:$false -Verbose
   }
