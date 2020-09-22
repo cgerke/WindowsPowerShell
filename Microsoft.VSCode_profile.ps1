7e5fea0278ad811cef0c017cc6733041fab0fc8f
chris.gerke@gmail.com <chris.gerke@gmail.com>
2020-09-22 19:56:49 +0800

    Insider bug workaround

--- Microsoft.VSCode_profile.ps1
+++ Microsoft.VSCode_profile.ps1
@@ -1,2 +1,2 @@
-# Learn more about this profile.
-
+# https://github.com/PowerShell/vscode-powershell/issues/742#issuecomment-301915916
+[System.Console]::OutputEncoding = [System.Text.Encoding]::ASCII
