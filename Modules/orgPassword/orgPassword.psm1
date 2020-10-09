a611bf0a35917f5ee4f283239552d1b835e52f63
Chris Gerke <chris.gerke@water.wa.gov.au>
2020-10-09 16:00:32 +0800

    Password tools

---
+++ Modules/orgPassword/orgPassword.psm1
@@ -0,0 +1,15 @@
+﻿function Get-RandomChar($length, $characters)
+{
+  $random = 1..$length | ForEach-Object { Get-Random -Maximum $characters.length }
+  $private:ofs = ""
+  return [String]$characters[$random]
+}
+
+function Get-Password
+{
+  $password = Get-RandomChar -length 5 -characters 'abcdefghiklmnoprstuvwxyz'
+  $password += Get-RandomChar -length 1 -characters 'ABCDEFGHKLMNOPRSTUVWXYZ'
+  $password += Get-RandomChar -length 1 -characters '1234567890'
+  $password += Get-RandomChar -length 1 -characters '!$%=?#*'
+  $password
+}
---
+++ Modules/orgUserPasswordExpiry/orgUserPasswordExpiry.psm1
@@ -0,0 +1,37 @@
+﻿function Get-PasswordExpiry
+{
+  <#
+  .SYNOPSIS
+    Wildcard search account password expiry.
+  .DESCRIPTION
+    Find Password Expiry date with a quick wildcard search in Active Directory. The wildcard can be any
+    combination of First name and Last name or the samAccountName.
+  .EXAMPLE
+    Get-PasswordExpiry "Chris"
+  .EXAMPLE
+    Get-PasswordExpiry "Gerke"
+  .EXAMPLE
+    Get-PasswordExpiry "Chris Gerke"
+  .EXAMPLE
+    Get-PasswordExpiry "gerkec"
+  #>
+  Param(
+    [Parameter(Position = 0, mandatory = $true)]
+    [string] $Name
+  )
+  $maxPasswordAge = (Get-ADDefaultDomainPasswordPolicy).MaxPasswordAge.Days
+  $displayName = Get-ADUser -Filter "displayName -like '*$Name*'" -Properties * |
+  Select-Object -Property "Displayname", PasswordLastSet, @{n = "ExpiryDate"; e = { $_.PasswordLastSet.AddDays($maxPasswordAge) } }
+
+  If ($displayName)
+  {
+    # Probably doing a First, Last or Display Name search.
+    $displayName
+  }
+  Else
+  {
+    # Probably doing a samAccount search.
+    Get-ADUser -Filter "samAccountName -like '$Name*'" -Properties * |
+    Select-Object -Property "Displayname", PasswordLastSet, @{n = "ExpiryDate"; e = { $_.PasswordLastSet.AddDays($maxPasswordAge) } }
+  }
+}
