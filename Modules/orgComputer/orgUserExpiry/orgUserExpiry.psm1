b8a94457e01b05e7d0f36bf7dcac1b06df42f310
Chris <chris.gerke@gmail.com>
2020-10-09 14:42:34 +0800

    Retrieve expiry date
    Retrieve Active Directory user account expiry date.

---
+++ Modules/orgComputer/orgUserExpiry/orgUserExpiry.psm1
@@ -0,0 +1,33 @@
+﻿function Get-Expiry
+{
+  <#
+.SYNOPSIS
+  Wildcard search account expiry.
+.DESCRIPTION
+  Find Expiry date with a quick wildcard search in Active Directory.
+.EXAMPLE
+  Get-Expiry "Chris"
+.EXAMPLE
+  Get-Expiry "Gerke"
+.EXAMPLE
+  Get-Expiry "Chris Gerke"
+.EXAMPLE
+  Get-Expiry "gerkec"
+#>
+  Param(
+    [Parameter(Position = 0, mandatory = $true)]
+    [string] $Name
+  )
+  $displayName = Get-ADUser -Properties * -Filter "displayName -like '*$Name*'" |
+    Select-Object GivenName, surname, company, department, description, AccountExpirationDate
+  If ($displayName)
+  {
+    # Probably doing a First, Last or Display Name search.
+    $displayName
+  }
+  Else
+  {
+    Get-ADUser -Properties * -Filter "samAccountName -like '*$Name*'" |
+      Select-Object GivenName, surname, company, department, description, AccountExpirationDate
+  }
+}
