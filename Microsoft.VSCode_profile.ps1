7ae87389da30e616c2d924c713374a9bc01d22f6
chris.gerke@gmail.com <chris.gerke@gmail.com>
2020-10-20 08:15:48 +0800

    Issue 742 Closed

--- Microsoft.VSCode_profile.ps1
+++ Microsoft.VSCode_profile.ps1
@@ -1,2 +1,2 @@
 # https://github.com/PowerShell/vscode-powershell/issues/742#issuecomment-301915916
-[System.Console]::OutputEncoding = [System.Text.Encoding]::ASCII
+#[System.Console]::OutputEncoding = [System.Text.Encoding]::ASCII
