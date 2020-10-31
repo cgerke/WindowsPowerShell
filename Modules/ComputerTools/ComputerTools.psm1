a0598a1b1bad1dfc757bd0dff512b9efc8524d97
chris.gerke@gmail.com <chris.gerke@gmail.com>
2020-10-31 16:35:57 +0800

    Simplify

--- Modules/ComputerTools/ComputerTools.psm1
+++ Modules/ComputerTools/ComputerTools.psm1
@@ -115,3 +115,25 @@ function Get-ComputerUptime
   }
 
 }
+
+Function Get-ToshibaWarranty
+{
+  <#
+    .SYNOPSIS
+      Toshiba computer warranty.
+    .DESCRIPTION
+      Get warranty information for Toshiba computers via a json response.
+    .EXAMPLE
+      Get-ToshibaWarranty -Serial $Serial
+    #>
+  Param(
+    [Parameter(Position = 0, mandatory = $true)]
+    [string] $Serial
+  )
+  $web = New-Object Net.WebClient
+  $url = "http://support.toshiba.com/support/warrantyResults?sno="
+  $url = $url + $Serial
+  $content = $web.DownloadString($url) | Out-String | ConvertFrom-Json
+  $warranty = $content.commonBean
+  $warranty
+}
