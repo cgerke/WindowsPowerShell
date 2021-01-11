46ab53e9e6f1ebbef90c3ef3a50c7733b0e8347b
Chris <chris.gerke@gmail.com>
2021-01-11 14:10:24 +0800

    Get-ComputerMacAddresses

--- Modules/ComputerTools/ComputerTools.psm1
+++ Modules/ComputerTools/ComputerTools.psm1
@@ -82,6 +82,46 @@ function Format-TimeSpan {
   }
 }
 
+function Get-ComputerMacAddresses
+{
+  <#
+    .SYNOPSIS
+      System mac addresses
+    .DESCRIPTION
+      Retrieve system mac addresses from a remote computer.
+    .EXAMPLE
+      Get-ComputerMacAddresses-Computer $hostname
+    #>
+  Param(
+    [Parameter(Position = 0, mandatory = $true, ValueFromPipeline = $true)]
+    [string[]] $Computer
+  )
+
+  process {
+    foreach ( $i in $Computer ) {
+      ### WinRM remoting by default
+      ### Enable-PSRemoting -SkipNetworkProfileCheck -Force
+      If (Test-WSMan -ComputerName $i -ErrorAction SilentlyContinue)
+      {
+        Write-Information "WinRM available."
+        $CimSession = New-CimSession -ComputerName $i
+      }
+      Else
+      {
+        # Use DCOM if WinRM is not available
+        $CimSessionOption = New-CimSessionOption -Protocol "DCOM"
+        $CimSession = New-CimSession -ComputerName $i -SessionOption $CimSessionOption
+      }
+
+
+      $CIMmac = Get-CimInstance -CimSession $CimSession -ClassName Win32_NetworkAdapterConfiguration | Where-Object {$_.MACAddress -ne $null} | Select-Object Description, MACAddress
+
+      return $CIMmac
+    }
+  }
+
+}
+
 function Get-ComputerUptime
 {
   <#
