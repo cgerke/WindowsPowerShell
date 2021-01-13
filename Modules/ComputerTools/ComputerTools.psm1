fedfa60f1a38bc42433cabbfbfc6adbe2c95c07c
Chris <chris.gerke@gmail.com>
2021-01-13 13:54:35 +0800

    New-WinPE

--- Modules/ComputerTools/ComputerTools.psm1
+++ Modules/ComputerTools/ComputerTools.psm1
@@ -241,6 +241,14 @@ function New-BackupUserProfile
 }
 
 function New-WinPE {
+  <#
+    .SYNOPSIS
+      Basic WinPE creation.
+    .DESCRIPTION
+      Setup a basic WinPE environment including powershell and tools.
+    .EXAMPLE
+      New-WinPE -Architecture amd64
+    #>
   param (
         [ValidateSet("amd64","x86")]
         [String]
@@ -277,17 +285,29 @@ function New-WinPE {
   # High performance profile
   Add-Content -Path "$peCache\mount\Windows\System32\startnet.cmd" -Value 'powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c'
 
-  #Powershell
+  # Startnet PowerShell
+  Add-Content -Path "$peCache\mount\Windows\System32\startnet.cmd" -Value 'X:\Windows\system32\WindowsPowerShell\v1.0\powershell -nologo -executionpolicy bypass'
+
+  #$profile
+  Add-Content -Path "$peCache\mount\Windows\System32\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -Value 'Set-Location X:\Windows\system32'
   Add-Content -Path "$peCache\mount\Windows\System32\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -Value 'hostname'
   Add-Content -Path "$peCache\mount\Windows\System32\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -Value 'Get-Disk | Where-Object {$_.bustype -ne "USB"}'
-  Add-Content -Path "$peCache\mount\Windows\System32\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -Value 'Set-Location X:\Windows\system32'
-  Add-Content -Path "$peCache\mount\Windows\System32\startnet.cmd" -Value 'X:\Windows\system32\WindowsPowerShell\v1.0\powershell -nologo -executionpolicy bypass'
+  Add-Content -Path "$peCache\mount\Windows\System32\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -Value 'dir *.ps1'
 
   # Utilities
   Add-Content -Path "$peCache\mount\Windows\System32\erase.ps1" -Value 'Get-Disk | Where-Object {$_.bustype -ne "USB"} | Foreach-Object {'
   Add-Content -Path "$peCache\mount\Windows\System32\erase.ps1" -Value '$command = @"'
+  Add-Content -Path "$peCache\mount\Windows\System32\erase.ps1" -Value 'list disk'
+  Add-Content -Path "$peCache\mount\Windows\System32\erase.ps1" -Value 'list volume'
   Add-Content -Path "$peCache\mount\Windows\System32\erase.ps1" -Value 'select disk $($_.Number)'
   Add-Content -Path "$peCache\mount\Windows\System32\erase.ps1" -Value 'clean all'
+  Add-Content -Path "$peCache\mount\Windows\System32\erase.ps1" -Value 'create partition primary'
+  Add-Content -Path "$peCache\mount\Windows\System32\erase.ps1" -Value 'select parition 1'
+  Add-Content -Path "$peCache\mount\Windows\System32\erase.ps1" -Value 'active'
+  Add-Content -Path "$peCache\mount\Windows\System32\erase.ps1" -Value 'format FS=NTFS quick label=CLEANED'
+  Add-Content -Path "$peCache\mount\Windows\System32\erase.ps1" -Value 'list disk'
+  Add-Content -Path "$peCache\mount\Windows\System32\erase.ps1" -Value 'list volume'
+  Add-Content -Path "$peCache\mount\Windows\System32\erase.ps1" -Value 'exit'
   Add-Content -Path "$peCache\mount\Windows\System32\erase.ps1" -Value '"@'
   Add-Content -Path "$peCache\mount\Windows\System32\erase.ps1" -Value '$command | diskpart'
   Add-Content -Path "$peCache\mount\Windows\System32\erase.ps1" -Value '}'
