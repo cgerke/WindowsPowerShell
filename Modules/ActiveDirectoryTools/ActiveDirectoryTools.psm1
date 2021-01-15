001e3d852c37f2cb2a508869b643664357557fc4
Chris <chris.gerke@gmail.com>
2021-01-15 13:52:02 +0800

    1 less funcion

--- Modules/ActiveDirectoryTools/ActiveDirectoryTools.psm1
+++ Modules/ActiveDirectoryTools/ActiveDirectoryTools.psm1
@@ -219,18 +219,19 @@ function Get-GroupMember
   Get-GroupRecurse "$Name" | Sort-Object Group
 }
 
-function Get-RandomChar($length, $characters)
-{
-  $random = 1..$length | ForEach-Object { Get-Random -Maximum $characters.length }
-  $private:ofs = ""
-  return [String]$characters[$random]
-}
-
 function Get-Password
 {
-  $password = Get-RandomChar -length 5 -characters 'abcdefghikmnprstuvwxyz'
-  $password += Get-RandomChar -length 1 -characters 'ABCDEFGHKMNPRSTUVWXYZ'
-  $password += Get-RandomChar -length 1 -characters '23456789'
-  $password += Get-RandomChar -length 1 -characters '!$%=?#*'
-  $password
+  <#
+  .SYNOPSIS
+    Generate a random complex password.
+  .DESCRIPTION
+    Generate a random complex password, 8 chars in length containing, 1 upper case char, 1 integer, 1 special char
+  .EXAMPLE
+    Get-Password
+  #>
+  $password = -join ((97..122) | Get-Random -Count 5 | ForEach-Object {[char]$_}) #ASCII lower case
+  $password += -join ((65..90) | Get-Random -Count 1 | ForEach-Object {[char]$_}) #ASCII upper case
+  $password += -join ((1..9) | Get-Random -Count 1 | ForEach-Object {$_}) #Integer
+  $password += -join ((33,35,6,37) | Get-Random -Count 1 | ForEach-Object {[char]$_}) #ASCII !#$%
+  return $password
 }
