d6eae48d62201f55fb76aef3a8cb579232c1a5a7
Chris <cgerke@outlook.com>
2021-03-05 15:39:07 +0800

    Splitting. Making things more modular.

--- Modules/ComputerTools/ComputerTools.psm1
+++ Modules/ComputerTools/ComputerTools.psm1
@@ -83,46 +83,6 @@ function Format-TimeSpan {
   }
 }
 
-function Get-MacAddress
-{
-  <#
-    .SYNOPSIS
-      System mac addresses
-    .DESCRIPTION
-      Retrieve system mac addresses from a remote computer.
-    .EXAMPLE
-      Get-MacAddress -Computer $hostname
-    #>
-  Param(
-    [Parameter(Position = 0, mandatory = $true, ValueFromPipeline = $true)]
-    [string[]] $Computer
-  )
-
-  process {
-    foreach ( $i in $Computer ) {
-      ### WinRM remoting by default
-      ### Enable-PSRemoting -SkipNetworkProfileCheck -Force
-      If (Test-WSMan -ComputerName $i -ErrorAction SilentlyContinue)
-      {
-        Write-Information "WinRM available."
-        $CimSession = New-CimSession -ComputerName $i
-      }
-      Else
-      {
-        # Use DCOM if WinRM is not available
-        $CimSessionOption = New-CimSessionOption -Protocol "DCOM"
-        $CimSession = New-CimSession -ComputerName $i -SessionOption $CimSessionOption
-      }
-
-
-      $CIMmac = Get-CimInstance -CimSession $CimSession -ClassName Win32_NetworkAdapterConfiguration | Where-Object {$null -ne $_.MACAddress} | Select-Object Description, MACAddress
-
-      return $CIMmac
-    }
-  }
-
-}
-
 function Get-ComputerUptime
 {
   <#
---
+++ Modules/macAddress/macAddress.psm1
@@ -0,0 +1,37 @@
+﻿function Get-MacAddress
+{
+  <#
+    .SYNOPSIS
+      Retrieve macaddresses
+    .DESCRIPTION
+      Retrieve macaddresses from a remote computer.
+    .EXAMPLE
+      Get-MacAddress -Computer $hostname
+    #>
+  Param(
+    [Parameter(Position = 0, mandatory = $true, ValueFromPipeline = $true)]
+    [string[]] $Computer
+  )
+
+  process {
+    foreach ( $i in $Computer ) {
+      ### WinRM remoting by default
+      ### Enable-PSRemoting -SkipNetworkProfileCheck -Force
+      If (Test-WSMan -ComputerName $i -ErrorAction SilentlyContinue)
+      {
+        Write-Information "WinRM available."
+        $CimSession = New-CimSession -ComputerName $i
+      }
+      Else
+      {
+        # Use DCOM if WinRM is not available
+        $CimSessionOption = New-CimSessionOption -Protocol "DCOM"
+        $CimSession = New-CimSession -ComputerName $i -SessionOption $CimSessionOption
+      }
+
+      $CIMmac = Get-CimInstance -CimSession $CimSession -ClassName Win32_NetworkAdapterConfiguration | Where-Object {$null -ne $_.MACAddress} | Select-Object Description, MACAddress
+
+      return $CIMmac
+    }
+  }
+}
