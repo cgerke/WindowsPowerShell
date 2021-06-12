098db67e1202a21566c3fd535fd457b9c8ac2073
Chris <chris.gerke@gmail.com>
2021-06-12 20:07:24 +0800

    VScode integrated terminal POSH

--- Microsoft.VSCode_profile.ps1
+++ Microsoft.VSCode_profile.ps1
@@ -1,2 +1,6 @@
 # https://github.com/PowerShell/vscode-powershell/issues/742#issuecomment-301915916
 #[System.Console]::OutputEncoding = [System.Text.Encoding]::ASCII
+Import-Module -Name PSReadline -Version 2.0.0
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme ys
