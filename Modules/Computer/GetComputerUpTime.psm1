3437271e8cae3192da672cbf5e54c2ea79a89661
Chris <cgerke@outlook.com>
2021-07-09 09:26:12 +0800

    Reuse code!

--- Modules/Computer/GetComputerUpTime.psm1
+++ Modules/Computer/GetComputerUpTime.psm1
@@ -17,30 +17,7 @@
     {
         foreach ( $i in $Computer )
         {
-            ### WinRM remoting by default
-            ### Enable-PSRemoting -SkipNetworkProfileCheck -Force
-            If (Test-WSMan -ComputerName $i -ErrorAction SilentlyContinue)
-            {
-                Write-Information "WinRM available." -InformationAction Continue
-                $CimSession = New-CimSession -ComputerName $i
-            }
-            Else
-            {
-                Write-Information "Using DCOM as WinRM is not available" -InformationAction Continue
-                $CimSessionOption = New-CimSessionOption -Protocol "DCOM"
-                $CimSession = New-CimSession -ComputerName $i -SessionOption $CimSessionOption
-            }
-
-            $CIMopsys = Get-CimInstance -CimSession $CimSession -ClassName Win32_OperatingSystem
-            $ComputerObject = [PSCustomObject]@{
-                Name                   = $i
-                LastBootUpTime         = $CIMopsys.LastBootUpTime
-                LastBootUpTimeRelative = (Get-Date) - $CIMopsys.LastBootUpTime | Format-TimeSpan
-            }
-
-            Remove-CimSession -ComputerName $i
-
-            return $ComputerObject
+            Get-Computer -Computer $Computer | Select-Object LastBootUpTime,LastBootUpTimeRelative
         }
     }
 }
