12dc39c6d5c5f103d1c0d9b152559f3c241e8fa1
Chris <cgerke@outlook.com>
2021-07-09 09:17:03 +0800

    Reuse code...doh

--- Modules/Computer/GetComputerUserprofileAge.psm1
+++ Modules/Computer/GetComputerUserprofileAge.psm1
@@ -20,32 +20,7 @@ function Get-ComputerUserprofileAge
     {
         foreach ( $i in $Computer )
         {
-            if (-not (Test-Connection -Quiet -ComputerName "$Computer" -Count 2))
-            {
-                Write-Information "$Computer appears to be offline" -InformationAction Continue
-            }
-            else
-            {
-                ### WinRM remoting by default
-                ### Enable-PSRemoting -SkipNetworkProfileCheck -Force
-                if (Test-WSMan -ComputerName $i -ErrorAction SilentlyContinue)
-                {
-                    Write-Information "WinRM available" -InformationAction Continue
-                    $CimSession = New-CimSession -ComputerName $i
-                }
-                else
-                {
-                    Write-Information "Using DCOM as WinRM is not available" -InformationAction Continue
-                    $CimSessionOption = New-CimSessionOption -Protocol "DCOM"
-                    $CimSession = New-CimSession -ComputerName $i -SessionOption $CimSessionOption
-                }
-
-                $ComputerObject = Get-CimInstance -CimSession $CimSession -ClassName Win32_Userprofile | Select-Object lastusetime, localpath, sid
-
-                Remove-CimSession -ComputerName $i
-
-                return $ComputerObject
-            }
+            $(Get-Computer -Computer $Computer).Userprofile
         }
     }
 }
