89de4f09f018934ccea3ccd5f60ac329c7ace189
Chris <cgerke@outlook.com>
2021-07-16 11:44:21 +0800

    Templates

---
+++ Modules/Templates/HashTable.psm1
@@ -0,0 +1,43 @@
+﻿function Get-WithHashTable {
+    <#
+      .SYNOPSIS
+          Summary
+      .DESCRIPTION
+          Detail
+      .EXAMPLE
+          Get-WithHashTable
+      .EXAMPLE
+          Get-WithHashTable -JSON
+      .EXAMPLE
+          $(Get-WithHashTable).Property
+
+      #>
+      Param(
+          [Parameter(Position = 0, mandatory = $false)]
+          [switch] $JSON
+      )
+
+    $HashTable = @{
+      "Node1" = @{
+        Leaf = "Data1"
+      }
+      "Node2" = @{
+        Leaf = "Data2"
+        Leaf2 = "Data3"
+        ExpandedLeaf = @{
+          "ExLeaf3" = "Data4"
+        }
+      }
+    }
+
+    If($PSBoundParameters.ContainsKey("JSON")) {
+      $OrderedHashtable = [ordered]@{}
+      foreach ($Item in ($HashTable.GetEnumerator() | Sort-Object -Property Key)) {
+          $OrderedHashtable[$Item.Key] = $Item.Value
+      }
+      $OrderedHashtable | ConvertTo-Json
+    } Else {
+      return $HashTable
+    }
+
+  }
---
+++ Modules/Templates/Pipeline.psm1
@@ -0,0 +1,25 @@
+﻿function Get-WithPipeline
+{
+    <#
+  .SYNOPSIS
+    Summary
+  .DESCRIPTION
+    Detail
+  .EXAMPLE
+    Get-WithPipeline -Object "object"
+  .EXAMPLE
+    "object","object2" | Get-WithPipeline
+  #>
+    Param(
+        [Parameter(Position = 0, mandatory = $true, ValueFromPipeline = $true)]
+        [string] $Object
+    )
+
+    process
+    {
+        foreach ( $i in $Object )
+        {
+            Write-Host $Object
+        }
+    }
+}
