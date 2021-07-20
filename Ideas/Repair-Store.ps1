01faa4fe3147068f23c2a9e3dc42ab52ccf94ab4
Chris <cgerke@outlook.com>
2021-07-20 14:27:27 +0800

    Naming for finding

--- Ideas/Repair-Store.ps1
+++ Ideas/Repair-Store.ps1
@@ -1,7 +1,6 @@
 # Repair the store
 Get-AppXPackage *WindowsStore* -AllUsers | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
-Get-Appxpackage –Allusers
-Microsoft.MPEG2VideoExtension_1.0.22661.0_x64__8wekyb3d8bbwe
+Get-Appxpackage –Allusers -Name Microsoft.MPEG2VideoExtension_1.0.22661.0_x64__8wekyb3d8bbwe
 Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.MPEG2VideoExtension_1.0.22661.0_x64__8wekyb3d8bbwe" –DisableDevelopmentMode
 Get-AppxPackage -allusers Microsoft.WindowsStore | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
 
